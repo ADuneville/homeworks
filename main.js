22391089ac5a14f54be2d82a48e5e0bf9a1f9d37
ADuneville <dop3l@mail.ru>
2020-05-30 21:21:36 +0300

    1-я домашняя работа

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+//Задание 1
 
+let algebra = 5;
+let geography = 4;
+let physics = 4; 
+let mean = (algebra + geography + physics) / 3;
+
+console.log(mean);
+
+
+console.log();
+//Задание 2 
+let name = 'Степан';
+let message = `Привет, мир! Меня зовут ${name}`; 
+
+console.log(message);
